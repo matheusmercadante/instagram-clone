9d5c11f63c4ab4a51d8e2963db00042109d71191
Matheus Mercadadnte <matheusmercadante2002@gmail.com>
2022-03-12 01:27:33 -0300

    feat: init application

---
+++ ms-media/src/main/java/com/clone/instagram/msmedia/MsMediaApplication.java
@@ -0,0 +1,13 @@
+package com.clone.instagram.msmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMediaApplication.class, args);
+	}
+
+}
---
+++ ms-media/src/test/java/com/clone/instagram/msmedia/MsMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.instagram.msmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
