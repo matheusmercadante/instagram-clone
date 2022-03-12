467c001c1dd700b99994419ea1a35ddff6560d19
Matheus Mercadadnte <matheusmercadante2002@gmail.com>
2022-03-12 01:28:34 -0300

    feat: add file exceptions

---
+++ ms-media/src/main/java/com/clone/instagram/msmedia/exception/InvalidFileNameException.java
@@ -0,0 +1,15 @@
+package com.clone.instagram.msmedia.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidFileNameException extends RuntimeException {
+  public InvalidFileNameException(String message) {
+    super(message);
+  }
+
+  public InvalidFileNameException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
