d98b1684539676972a2e47e0d134b6a70ac78fb9
Matheus Mercadadnte <matheusmercadante2002@gmail.com>
2022-03-13 16:19:21 -0300

    test: add main application test

---
+++ ms-follow/src/test/java/com/clone/instagram/msfollow/MsFollowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.instagram.msfollow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFollowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
