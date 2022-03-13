37781cad83db36f46ec74357cb27c936d306438e
Matheus Mercadadnte <matheusmercadante2002@gmail.com>
2022-03-13 16:41:38 -0300

    feat: init

---
+++ ms-feed/src/main/java/com/clone/instagram/msfeed/MsFeedApplication.java
@@ -0,0 +1,13 @@
+package com.clone.instagram.msfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFeedApplication.class, args);
+	}
+
+}
---
+++ ms-feed/src/test/java/com/clone/instagram/msfeed/MsFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.instagram.msfeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
