712acdf403620a232d40e9873510667dbb2c53ba
Matheus Mercadadnte <matheusmercadante2002@gmail.com>
2022-03-13 16:19:36 -0300

    feat: add main application init

---
+++ ms-follow/src/main/java/com/clone/instagram/msfollow/MsFollowApplication.java
@@ -0,0 +1,13 @@
+package com.clone.instagram.msfollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFollowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsFollowApplication.class, args);
+	}
+
+}
