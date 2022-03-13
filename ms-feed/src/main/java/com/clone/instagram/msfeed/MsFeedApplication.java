a13d923ccc3083020de1f91c68a1cbeae31f88e4
Matheus Mercadadnte <matheusmercadante2002@gmail.com>
2022-03-13 17:32:12 -0300

    feat: add and enable feign clients

--- ms-feed/src/main/java/com/clone/instagram/msfeed/MsFeedApplication.java
+++ ms-feed/src/main/java/com/clone/instagram/msfeed/MsFeedApplication.java
@@ -2,12 +2,21 @@ package com.clone.instagram.msfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsFeedApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsFeedApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
